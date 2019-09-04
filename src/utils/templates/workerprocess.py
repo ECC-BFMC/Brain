ecac6141e79d23622ebba69a870ff7c101be2682
Nandor Kilyen <Nandor.Kilyen2@ro.bosch.com>
2019-09-04 14:54:14 +0300

    Add stop method and change the terminating mechanism to avoid a deadlock.

--- src/utils/templates/workerprocess.py
+++ src/utils/templates/workerprocess.py
@@ -1,5 +1,5 @@
 import multiprocessing
-from multiprocessing import Process
+from multiprocessing import Process, Event
 
 class WorkerProcess(Process):
 
@@ -23,6 +23,8 @@ class WorkerProcess(Process):
         self.daemon = daemon
         self.threads = list()
 
+        self._blocker = Event()
+
     
     def _init_threads(self):
         """ It initializes the threads of the process and adds the thread to the 'threads' list, which will be automatically started and stopped in the 'run' method.
@@ -38,11 +40,31 @@ class WorkerProcess(Process):
         """This method applies the initialization of the theards and starts all of them. The process ignores the keyboardInterruption signal and can terminate by applying the 'stop' method. 
         The process will be blocked, until an other process use the 'stop' function. After appling the function it terminates all subthread.
         """
-        self.init_threads()
-
+        self._init_threads()
         for th in self.threads:
             th.daemon = self.daemon
             th.start()
-
+        
+        # Wait to set internal flag true for the event
+        while not self._blocker.is_set():
+            try:
+                self._blocker.wait()
+            except KeyboardInterrupt: # Ignoring the KeyboardInterrupt signal.
+                pass
+        
         for th in self.threads:
-            th.join()
+            if hasattr(th,'stop') and callable(getattr(th,'stop')):
+                th.stop()
+                th.join(0.1)
+                if th.is_alive():
+                    print("The thread %s cannot normally stop, it's blocked somewhere!"%(th))
+                    del th
+            else:
+                del th
+    
+    def stop(self):
+        """This method stops the process by set the event, which has role to block the running of process, while the subthread executes their functionalities. 
+        The main process or other process throught this method can stop the running of this process.
+        """
+        self._blocker.set()
+        
