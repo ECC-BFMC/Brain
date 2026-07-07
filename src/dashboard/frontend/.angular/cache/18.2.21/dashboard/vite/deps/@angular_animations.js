b5cb6ccbddc2ee8891d45bf998c1905d517752dc
bogdan <139842335+balcaubogdanbosch@users.noreply.github.com>
2026-07-07 16:15:27 +0300

    added docker files

--- src/dashboard/frontend/.angular/cache/18.2.21/dashboard/vite/deps/@angular_animations.js
+++ src/dashboard/frontend/.angular/cache/18.2.21/dashboard/vite/deps/@angular_animations.js
@@ -23,8 +23,8 @@ import {
 } from "./chunk-BIM57T4P.js";
 import "./chunk-72LCDNJI.js";
 import "./chunk-KIS4PIWY.js";
-import "./chunk-PEBH6BBU.js";
 import "./chunk-WPM5VTLQ.js";
+import "./chunk-PEBH6BBU.js";
 import "./chunk-4S3KYZTJ.js";
 import "./chunk-N6ESDQJH.js";
 export {
--- src/dashboard/frontend/.angular/cache/18.2.21/dashboard/vite/deps/@angular_core.js
+++ src/dashboard/frontend/.angular/cache/18.2.21/dashboard/vite/deps/@angular_core.js
@@ -462,8 +462,8 @@ import {
   ɵɵviewQuery,
   ɵɵviewQuerySignal
 } from "./chunk-KIS4PIWY.js";
-import "./chunk-PEBH6BBU.js";
 import "./chunk-WPM5VTLQ.js";
+import "./chunk-PEBH6BBU.js";
 import "./chunk-4S3KYZTJ.js";
 import "./chunk-N6ESDQJH.js";
 export {
